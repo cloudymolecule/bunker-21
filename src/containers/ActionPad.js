8e2143330cdfaa37c5ba82abd8c45d4075c74930
Cloudymolecule <baires82@gmail.com>
2021-12-07 13:45:31 -0500

    Add components

---
+++ src/containers/ActionPad.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ActionPad extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default ActionPad
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,9 @@ import '../styles/App.css'
 import CompSysTitle from '../components/CompSysTitle'
 import Warning from './Warning'
 import DataPad from './DataPad'
+import Camera from './Camera'
+import Message from './Message'
+import ActionPad from './ActionPad'
 
 function App() {
   return (
@@ -10,7 +13,11 @@ function App() {
       <Warning />
       <div>
         <DataPad />
+        <Camera />
       </div>
+      <Message />
+      <ActionPad />
+
     </div>
   )
 }
