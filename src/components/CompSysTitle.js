4c7c6952f0a0cde8c83f56fa16120d8bf455f226
Cloudymolecule <baires82@gmail.com>
2021-12-08 11:23:36 -0500

    Make title flicker

--- src/components/CompSysTitle.js
+++ src/components/CompSysTitle.js
@@ -2,11 +2,44 @@ import React from 'react'
 import '../styles/CompSysTitle.css'
 
 function CompSysTitle() {
+    
+    function flicker() { // this will flicker the title
+        
+        function randomInterval(min, max) {
+            return Math.floor(Math.random() * (max - min + 1) + min)
+        }
+
+        function quicklySwitchClass() {
+            const element = document.getElementById('comp-sys-title')
+            for (let i = 1; i < randomInterval(2, 8); i++) {
+                setTimeout(() => {
+                    if (element.className === 'solid') {
+                        element.className = 'flicker'
+                    } else {
+                        element.className = 'solid'
+                    }
+                }, 100);
+            }
+            element.className = 'solid'
+            console.log(element.className)
+
+        }
+
+        setInterval(() => {
+            quicklySwitchClass()
+        }, randomInterval(1000, 20000));
+    }
+
     return (
-        <div className='comp-sys-title'>
-            BUNKER COMPUTER SYSTEMS V2.1.2
+        <div className='comp-sys-title-container'>
+            <div className='solid' id='comp-sys-title'>
+                {flicker()}
+                BUNKER COMPUTER SYSTEMS V2.1.2
+            </div>
         </div>
     )
 }
 
 export default CompSysTitle
+
+// BUNKER COMPUTER SYSTEMS V2.1.2
