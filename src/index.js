a2d99320485ce520215135f6c43d8266a717f2d7
Cloudymolecule <baires82@gmail.com>
2021-12-15 11:51:08 -0500

    Install Redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import ReactDOM from 'react-dom'
 import './styles/index.css'
 import App from './containers/App'
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import combineReducers from './reducers/index'
+
+const middleware = [thunk]
+
+let store = createStore(combineReducers, composeWithDevTools(
+  applyMiddleware(...middleware)
+))
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+document.getElementById('root')
 )
