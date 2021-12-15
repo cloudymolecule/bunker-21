a5fc2d3a75c1936f5e884e1cccac39b8321835ff
Cloudymolecule <baires82@gmail.com>
2021-12-15 12:59:24 -0500

    Update Time.js

--- src/components/data-pad/Time.js
+++ src/components/data-pad/Time.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class Time extends Component {
     
     state = {
-        hours: '',
+        hours: 0,
         minutes: 0,
         seconds: 0
     }
@@ -12,23 +12,33 @@ class Time extends Component {
         setInterval(() => {
             if (this.state.seconds === 59) {
                 this.setState({
+                    seconds: 0,
                     minutes: this.state.minutes + 1
                 })
+                
             }
-
-
-
+            if (this.state.minutes === 59) {
+                this.setState({
+                    minutes: 0,
+                    hours: this.state.hours + 1
+                })
+            }
+            if (this.state.hours === 23) {
+                this.setState({
+                    hours: 0,
+                })
+            }
 
             this.setState({
                 seconds: this.state.seconds + 1
             })
         }, 1000);
     }
-    
+
     render() {
         return (
             <div>
-                {this.state.seconds}
+                {/* {this.state.hours}:{this.state.minutes}:{this.state.seconds} */}
             </div>
         )
     }
