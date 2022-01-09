0af373d62d355db39447a5adce61b06f6c3d7538
Dario Carlino <baires82@gmail.com>
2022-01-09 12:49:15 -0500

    Update Weather.js

--- src/components/data-pad/Weather.js
+++ src/components/data-pad/Weather.js
@@ -2,9 +2,22 @@ import React from 'react'
 import { WeatherGen } from '../../helpers/WeatherGen'
 
 function Weather() {
+    
+
+    function weatherInterval() {
+        setInterval(() => {
+            console.log('ertert')
+            WeatherGen()
+        }, 1000);
+    }
+
+    // function weatherInterval = setInterval() => {
+    //     WeatherGen()
+    // }, Math.floor(Math.random() * (1000 + 3000));
+    
     return (
         <div>
-           {WeatherGen()}
+           {weatherInterval()}
         </div>
     )
 }
--- src/helpers/WeatherGen.js
+++ src/helpers/WeatherGen.js
@@ -1,4 +1,5 @@
 export const WeatherGen = function weatherGen() {
+
     const wPatterns = [ // repeated patterns added to increase probability of random selection
         'Sunny/Clear', 'Sunny/Clear','Sunny/Clear','Sunny/Clear','Sunny/Clear','Sunny/Clear','Sunny/Clear','Sunny/Clear',
         'Partially cloudy', 'Partially cloudy','Partially cloudy','Partially cloudy','Partially cloudy','Partially cloudy','Partially cloudy',
@@ -11,15 +12,10 @@ export const WeatherGen = function weatherGen() {
         
     ]
 
-    function randomInterval(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) + min)
-    }
-    
     function selectPattern() {
-        
+        const i = Math.floor(Math.random() * (35 + 1))
+        return wPatterns[i]
     }
-    
-    setInterval(() => {
-        
-    }, 2000);
+
+    return selectPattern()
 }
