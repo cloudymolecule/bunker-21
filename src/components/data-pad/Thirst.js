8316f4a8e559edf699d2ac0384a17f079b5d83c8
Dario Carlino <baires82@gmail.com>
2022-01-13 21:34:36 -0500

    Update Thirst.js

--- src/components/data-pad/Thirst.js
+++ src/components/data-pad/Thirst.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react'
+import { thirstLevels } from '../../constants/thirstLevels'
 
 class Thirst extends Component {
     
     state = {
-        currentThirst: 
+        currentThirstLevel: thirstLevels[0]
+    }
+
+    componentDidMount() {
+        let ind = 1
+
+        const thirstInterval = setInterval(() => {
+            if (ind === 7) {
+                clearInterval(thirstInterval)
+            }
+            this.setState({
+                currentThirstLevel: thirstLevels[ind]
+            })
+            ind ++
+        }, 1000)
+
     }
 
     render() {
         return (
             <div>
-                
+                {this.state.currentThirstLevel}
             </div>
         )
     }
