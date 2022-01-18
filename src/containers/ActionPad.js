c9abab92fa3a21c547eafc19b77af5dd90899002
Cloudymolecule <baires82@gmail.com>
2022-01-18 14:20:18 -0500

    Add switchComponents function

--- src/containers/ActionPad.js
+++ src/containers/ActionPad.js
@@ -7,14 +7,34 @@ import Terminal from '../components/action-pad/Terminal'
 import { Flicker } from '../helpers/Flicker'
 
 class ActionPad extends Component {
+    
+    state = {
+        compDisplayed: 0
+    }
+
     render() {
+
+        let switchComponents = () => {
+            if (this.state.compDisplayed === 3) {
+                this.setState({
+                    compDisplayed: 0
+                })
+            } else {
+                this.setState({
+                    compDisplayed: this.state.compDisplayed + 1
+                })
+            }
+        }
+
         return (
             <div className='action-pad-container' id='action-pad-container'>
                 {Flicker('action-pad-container')}
-                <Terminal />
+                <button id='action-pad-button' onClick={switchComponents}>switch</button>
+
+                {/* <Terminal />
                 <Inventory />
                 <LoadSaveEtc />
-                <Robots />
+                <Robots /> */}
             </div>
         )
     }
